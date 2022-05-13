d9a20a20c0fccd8873af6b090f3b096a31837e62
Leonardo16AM <leo16am@gmail.com>
2022-05-13 19:34:01 -0400

    Added multiple search and text path

--- MoogleEngine/Moogle.cs
+++ MoogleEngine/Moogle.cs
@@ -4,19 +4,19 @@
 
 public static class Moogle{
     public static SearchResult Query(string query) {
-
+        Console.WriteLine("===================Making a new search=======================");
         model model= new model();
         model.build_from_txts();
         // model.print();
 
+        List<vector> result= model.naive_search(query);
+
 
         string real_query=model.recomendation(query);
-        
-
         SearchItem[] items = new SearchItem[3] {
-            new SearchItem("Hello World", model.naive_search(query), 0.9f),
-            new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.5f),
-            new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.1f),
+            new SearchItem(result[0].path.Substring(11), result[0].full_text, 0.9f),
+            new SearchItem(result[1].path.Substring(11), result[1].full_text, 0.5f),
+            new SearchItem(result[2].path.Substring(11), result[2].full_text, 0.1f),
         };
 
         return new SearchResult(items, real_query);
---
+++ MoogleEngine/utils/vector.cs
@@ -0,0 +1,38 @@
+using System;
+
+public class vector{
+    public string path="placeholder.txt";
+    public string full_text="placeholder_text";
+    public List<string> words=new List<string>();
+    public Dictionary<string,int> wordcount= new Dictionary<string,int>();
+    public List<double> vec=new List<double>();
+    public double angle_with=100.0;
+
+
+    public bool find(string s){
+        return wordcount.ContainsKey(s);
+    }
+    public int frecuency(string s){
+        if(wordcount.ContainsKey(s))return wordcount[s];
+        else return 0;
+    }
+    public double dot_product(vector B){
+        double dot=0;
+        for(int i=0;i<vec.Count;i++){
+            dot += B.vec[i] * vec[i];
+        }
+        return dot;
+    }
+    public double module(){
+        double dot=0;
+        for(int i=0;i<vec.Count;i++){
+            dot += vec[i] * vec[i];
+        }
+        return Math.Sqrt(dot);
+    }
+
+    public double angle(vector B){
+        return Math.Acos( dot_product(B)/(module()*B.module()) );
+    }
+
+}
